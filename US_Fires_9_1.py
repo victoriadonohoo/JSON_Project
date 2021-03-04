98a5c5dc483fc32dbdbb43ea0745bc63a046059a
Victoria Donohoo <victoria_donohoo1@baylor.edu>
2021-03-03 19:34:09 -0600

    json project work

---
+++ US_Fires_9_1.py
@@ -0,0 +1,49 @@
+import json
+
+infile = open("US_fires_9_1.json", "r")
+outfile = open("readable_fire_data_9_1.json", "w")
+
+fire_data = json.load(infile)
+
+json.dump(fire_data, outfile, indent=4)
+
+lons, lats = [], []
+brights = []
+
+for fire in fire_data:
+    lon = float(fire["longitude"])
+    lat = float(fire["latitude"])
+    bright = float(fire["brightness"])
+    lons.append(lon)
+    lats.append(lat)
+    if fire["brightness"] > 450:
+        bright = fire["brightness"]
+        brights.append(bright)
+
+
+print(lons)
+print(lats)
+print(brights)
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+data = [
+    {
+        "type": "scattergeo",
+        "lon": lons,
+        "lat": lats,
+        "marker": {
+            "size": [0.02 * bright for bright in brights],
+            "color": brights,
+            "colorscale": "Viridis",
+            "reversescale": True,
+            "colorbar": {"title": "Brightness"},
+        },
+    }
+]
+
+
+my_layout = Layout(title="US Fires")
+fig = {"data": data, "layout": my_layout}
+offline.plot(fig, filename="USFires_9_1.html")
